e29e9ff1ee2b7c6613952864d69325d57404e483
Nickitarius <pilnov.nickita@yandex.ru>
2024-07-09 23:38:31 +0800

    s

--- front/src/components/TrainingPrograms.vue
+++ front/src/components/TrainingPrograms.vue
@@ -1,28 +1,32 @@
 <template>
+  <div>
     <h2 class="display-6">Программы тренировок</h2>
     <ul class="list-group list-group-flush list-group-item-action">
-
-        <router-link class="list-group-item list-group-item-action" v-for="program in programs" v-bind:key="program.id"
-            :to="{ name: 'trainingProgram', params: { id: program.id } }" v-text="program.name"></router-link>
-
+      <router-link
+        class="list-group-item list-group-item-action"
+        v-for="program in programs"
+        v-bind:key="program.id"
+        :to="{ name: 'trainingProgram', params: { id: program.id } }"
+        v-text="program.name"
+      ></router-link>
     </ul>
+  </div>
 </template>
 <script>
-
 export default {
-    name: 'TrainingPrograms',
-    data() {
-        return {
-            programs: [],
-        }
+  name: "TrainingPrograms",
+  data() {
+    return {
+      programs: [],
+    };
+  },
+  methods: {
+    getPrograms() {
+      return this.$store.getters.getDefaultData.training_programs;
     },
-    methods: {
-        getPrograms() {
-            return this.$store.getters.getDefaultData.training_programs;
-        }
-    },
-    mounted: function () {
-        this.programs = this.getPrograms();
-    }
-}
+  },
+  mounted: function () {
+    this.programs = this.getPrograms();
+  },
+};
 </script>
